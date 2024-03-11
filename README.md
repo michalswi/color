30322e31716c2acd53763f2047c2e0bfba018caa
ms <michal.swierczewski@nokia.com>
2024-03-11 14:21:40 +0100

    update func

---
+++ README.md
@@ -0,0 +1,25 @@
+# colorize
+
+![](https://img.shields.io/github/issues/michalswi/color)
+![](https://img.shields.io/github/forks/michalswi/color)
+![](https://img.shields.io/github/stars/michalswi/color)
+![](https://img.shields.io/github/last-commit/michalswi/color)
+
+```
+go get -u -v github.com/michalswi/color
+```
+
+### \# example
+```
+package main
+
+import "github.com/michalswi/color"
+
+func main() {
+	println(color.Format(color.RED, "red"))
+	println(color.Format(color.BLUE, "blue"))
+	println(color.Format(color.GREEN, "green"))
+	println(color.Format(color.YELLOW, "yellow"))
+	println(color.Format(color.PURPLE, "purple"))
+}
+```
--- main.go
+++ main.go
@@ -10,17 +10,17 @@ const escape = "\x1b"
 // Define color constants
 const (
 	NONE   = iota // No color
-	RED           // Red color
-	GREEN         // Green color
-	YELLOW        // Yellow color
-	BLUE          // Blue color
-	PURPLE        // Purple color
+	RED           // Red color (int 1)
+	GREEN         // Green color (int 2)
+	YELLOW        // Yellow color (int 3)
+	BLUE          // Blue color (int 4)
+	PURPLE        // Purple color (int 5)
 )
 
-// color function takes an integer c as input and
+// colorize function takes an integer c as input and
 // returns an escape sequence string for that color.
 // If c is NONE, it returns the reset sequence.
-func color(c int) string {
+func colorize(c int) string {
 	if c == NONE {
 		// Return the reset sequence
 		return fmt.Sprintf("%s[%dm", escape, c)
@@ -35,5 +35,5 @@ func color(c int) string {
 // for color c and the reset sequence.
 func Format(c int, text string) string {
 	// Wrap text with color c and reset sequence
-	return color(c) + text + color(NONE)
+	return colorize(c) + text + colorize(NONE)
 }
